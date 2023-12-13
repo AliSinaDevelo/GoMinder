dbb65c0a0cd14e636c7e19c6b5765f0b5db59c9f
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-13 23:55:56 +0100

    dependancies added

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+	"github.com/gen2brain/beeep"
+	"github.com/olebedev/when"
+	"github.com/olebedev/when/rules/common"
+	"github.com/olebedev/when/rules/en"
+)
+
+const (
+	markName = "GO_CLI_REMINDER"
+	markValue = "1"
+)
+
+func main() {
+	
